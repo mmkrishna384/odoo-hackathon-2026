8c76b3b7a3d5d934eb4493b77bc05a49bf1bf963
ESWAR.MANDAPATI <eswar.mandapati@ESWARMANDAPATIs-MacBook-Pro.local>
2026-07-12 14:07:46 +0530

    Allow dynamic CORS origins to support deployed frontend

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:3001'],
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
